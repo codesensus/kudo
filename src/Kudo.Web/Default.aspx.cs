291dd634bb4c04f8cea9a31e6076c378e231c98e
Troels Thomsen <tt@codesensus.com>
2009-11-01 10:00:30 +0100

    Create ASP.NET MVC project from template (and remove any unnecessary files)

---
+++ src/Kudo.Web/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿/**
+ * Copyright 2004-2009 Codesensus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Kudo.Web
+{
+	public partial class _Default : Page
+	{
+		public void Page_Load(object sender, System.EventArgs e)
+		{
+			// Change the current path so that the Routing handler can correctly interpret
+			// the request, then restore the original path so that the OutputCache module
+			// can correctly process the response (if caching is enabled).
+
+			string originalPath = Request.Path;
+			HttpContext.Current.RewritePath(Request.ApplicationPath, false);
+			IHttpHandler httpHandler = new MvcHttpHandler();
+			httpHandler.ProcessRequest(HttpContext.Current);
+			HttpContext.Current.RewritePath(originalPath, false);
+		}
+	}
+}
