1ffff70e7cb0fb70721eec7de4af20aaee5be0dc
Troels Thomsen <tt@codesensus.com>
2009-11-01 10:44:42 +0100

    Add requirement to password hash algorithm interface and describe with XML comments

--- src/Kudo.Web/Infrastructure/Security/IPasswordHashAlgorithm.cs
+++ src/Kudo.Web/Infrastructure/Security/IPasswordHashAlgorithm.cs
@@ -16,7 +16,30 @@
 
 namespace Kudo.Web.Infrastructure.Security
 {
+	/// <summary>
+	/// An interface for customizing the format of password hashes.
+	/// </summary>
+	/// <remarks>
+	/// The password hash algorithm SHOULD use a salt. If the algorithm uses
+	/// a salt, this MUST be included in the returned hash to allow the
+	/// verification method to read this value at a later point.
+	/// </remarks>
 	public interface IPasswordHashAlgorithm
 	{
+		/// <summary>
+		/// Generates a hash given a plain text password.
+		/// </summary>
+		/// <param name="password">The password to hash</param>
+		/// <returns>The hash</returns>
+		string GenerateHash(string password);
+
+		/// <summary>
+		/// Verifies that the given plain text validates against the given
+		/// hash.
+		/// </summary>
+		/// <param name="password">The password to verify</param>
+		/// <param name="hash">The hash</param>
+		/// <returns>true if the password is valid; otherwise, false</returns>
+		bool VerifyPassword(string password, string hash);
 	}
 }
