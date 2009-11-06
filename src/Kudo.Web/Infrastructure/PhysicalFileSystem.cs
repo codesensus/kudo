bd41c707d59396dc8e36ed93134420937bdc758c
Troels Thomsen <tt@codesensus.com>
2009-11-06 23:30:56 +0100

    Implement our physical file system class

--- src/Kudo.Web/Infrastructure/PhysicalFileSystem.cs
+++ src/Kudo.Web/Infrastructure/PhysicalFileSystem.cs
@@ -16,6 +16,8 @@
 
 using System;
 using System.IO;
+using System.Linq;
+using System.Web.Hosting;
 // We need to map this explicitly, as we've also got a File in the System.IO
 // namespace which we need to use
 using FileNode = Kudo.Core.DomainModel.File;
@@ -31,36 +33,92 @@ namespace Kudo.Web.Infrastructure
 			return string.Format("{0}" + separatorChar + "{1}", "media", folder);
 		}
 
+		/// <remarks>
+		/// This helper is only relevant as we translate paths using
+		/// HostingEnvironment.MapPath. If we were talking directly to the file
+		/// system Path.DirectorySeparatorChar should be used.
+		/// </remarks>
+		private string GetFilePath(FileNode file)
+		{
+			return GetFilePath(file, '/');
+		}
+
+		/// <remarks>
+		/// This helper is only relevant as we translate paths using
+		/// HostingEnvironment.MapPath. If we were talking directly to the file
+		/// system Path.DirectorySeparatorChar should be used.
+		/// </remarks>
+		private string GetFolderPath(FileNode file)
+		{
+			return GetFolderPath(file, '/');
+		}
+
+		protected virtual string MapPath(string virtualPath)
+		{
+			return HostingEnvironment.MapPath(virtualPath);
+		}
+
 		public override void Delete(FileNode file)
 		{
-			throw new NotImplementedException();
+			string folderPath = MapPath(GetFolderPath(file));
+			if (Directory.Exists(folderPath))
+			{
+				Directory.Delete(folderPath, true);
+			}
 		}
 
 		public override Stream Read(FileNode file)
 		{
-			throw new NotImplementedException();
+			string filePath = MapPath(GetFilePath(file));
+
+			return new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 		}
 
 		public override void Write(FileNode file, Stream input)
 		{
-			throw new NotImplementedException();
+			string folderPath = MapPath(GetFolderPath(file));
+
+			Directory.CreateDirectory(folderPath);
+
+			string filePath = MapPath(GetFilePath(file));
+
+			using (FileStream output = File.Create(filePath))
+			{
+				CopyStream(input, output);
+			}
 		}
 
 		public override Uri GetUrl(FileNode file)
 		{
-			throw new NotImplementedException();
+			return new Uri(string.Format("/{0}", GetFilePath(file, '/')), UriKind.Relative);
 		}
 
 		public override string ConvertToSafeFileName(string fileName)
 		{
-			throw new NotImplementedException();
+			char[] invalidFileNameChars = Path.GetInvalidFileNameChars();
+
+			var safeFileNameChars = (from c in fileName
+									 where !invalidFileNameChars.Contains(c)
+									 select c).ToArray();
+
+			return new string(safeFileNameChars);
 		}
 
 		public override bool SupportsBothHttpAndHttps
 		{
 			get
 			{
-				throw new NotImplementedException();
+				return true;
+			}
+		}
+
+		private static void CopyStream(Stream source, Stream destination)
+		{
+			var buffer = new byte[32768];
+			int read;
+			while ((read = source.Read(buffer, 0, buffer.Length)) > 0)
+			{
+				destination.Write(buffer, 0, read);
 			}
 		}
 	}
