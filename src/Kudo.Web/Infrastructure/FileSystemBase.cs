c25ff0042be5546d70c0d9adfe649917cbac64c8
Troels Thomsen <tt@codesensus.com>
2009-11-06 23:03:15 +0100

    Add base implementation for file systems

---
+++ src/Kudo.Web/Infrastructure/FileSystemBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Kudo.Data;
+using FileNode = Kudo.Core.DomainModel.File;
+
+namespace Kudo.Web.Infrastructure
+{
+	public abstract class FileSystemBase : IFileSystem
+	{
+		public void Delete(FileNode file)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Stream Read(FileNode file)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Write(FileNode file, Stream input)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Uri GetUrl(FileNode file)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string ConvertToSafeFileName(string fileName)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool SupportsBothHttpAndHttps
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
